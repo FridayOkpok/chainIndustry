3d83f3d1b1fac210c76181015f74a76fc3a6a4d8
Nathaniel-Etim <nathjoeetim@gmail.com>
2023-06-20 20:38:35 +0100

    build error

--- src/component/NavBar/orgName.jsx
+++ src/component/NavBar/orgName.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../image/logoimg.png";
+import logo from "../../image/logoImg.png";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
